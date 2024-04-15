565f534effbf044b84921434d24ace3b33f2760d
Admin <Admin@DESKTOP-AB6BR8M.lan>
2024-04-15 19:11:11 +0530

    New

--- Selenium_Qsp_Maven/src/test/java/Selenium/CMD_Parameter_.java
+++ Selenium_Qsp_Maven/src/test/java/Selenium/CMD_Parameter_.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class CMD_Parameter_test {
+public class CMD_Parameter_ {
 	
 	@Test
 	public void demo() throws InterruptedException {
